c84cf775fdc5a31857022b7e1fee75436003af3d
Karema <karemahamdy51@gmail.com>
2024-06-16 19:55:28 +0300

    5 problem

--- script.js
+++ script.js
@@ -158,4 +158,43 @@ class BankAccount {
   man.withdraw(260)
   man.deposit(240)
 
-  // Write a JavaScript program that creates a class called 'Shape' with a method to calculate the area. Create two subclasses, 'Circle' and 'Triangle', that inherit from the 'Shape' class and override the area calculation method. Create an instance of the 'Circle' class and calculate its area. Similarly, do the same for the 'Triangle' class.
+  // Write a JavaScript program that creates a class called 'Shape' with a method to calculate the area.
+  //  Create two subclasses, 'Circle' and 'Triangle', that inherit from the 'Shape' class and override the area calculation method. 
+  // Create an instance of the 'Circle' class and calculate its area. Similarly, do the same for the 'Triangle' class.
+
+  class Shape {
+    calculateArea(){
+    console.log("this method calc area");
+  }
+}
+class Circle extends Shape {
+  constructor(radius) {
+    super();
+    this.radius = radius;
+  }
+
+  calculateArea() {
+    return Math.PI * this.radius * this.radius;
+  }
+}
+
+class Rctangle extends Shape {
+  constructor(width, height) {
+    super();
+    this.width = width;
+    this.height = height;
+  }
+
+  calculateArea() {
+    return this.width * this.height;
+  }
+}
+
+const circle = new Circle(7);
+const circleArea = circle.calculateArea();
+console.log(`Circle Area: ${circleArea}`);
+
+// Create an instance of the Rectangle class
+const rectangle = new Rctangle(8, 9);
+const rectangleArea = rectangle.calculateArea();
+console.log(`Rectangle Area: ${rectangleArea}`);
